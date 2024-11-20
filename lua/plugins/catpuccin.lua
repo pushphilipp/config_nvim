93e7856dc616f23a5217d69459c5afb3f1d0bdba
pushphilipp <root@IMW1011150C.ad005.onehc.net>
2024-11-20 17:48:56 +0100

    Added transparency

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,10 +1,9 @@
 return {"catppuccin/nvim", name="catppuccin", priority = 1000,
   config = function() 
-    vim.cmd.colorscheme "catppuccin"
-  end
-
-
-
-
+  require("catppuccin").setup({
+    transparent_background = true, -- Enable transparency
+    })
+  vim.cmd.colorscheme "catppuccin"
+end
 }
 
